84ea40c7e373535355b216258ea50a55223d301c
Tianyou-Luo <157233355+Tianyou-Luo@users.noreply.github.com>
2024-10-08 02:17:00 -0400

    Add files via upload

---
+++ HW3 Design/wildlife_tracker/animal_management/animal_manager.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+from wildlife_tracker.animal_management.animal import Animal
+
+class AnimalManager:
+
+    def __init__(self) -> None:
+        animals: dict[int, Animal] = {}
+        
+    def assign_animals_to_habitat(self) -> None:
+        pass
+    
+    def get_animal_by_id(self, animal_id: int) -> Optional[Animal]:
+        pass
+    
+    def register_animal(self, Animal) -> None:
+        pass
+
+    def remove_animal(self, animal_id: int) -> None:
+        pass
